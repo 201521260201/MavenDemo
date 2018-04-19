79dc7d2bf3700cc99e288fef789a95e8d2544eb0
ligaoxiang <571299065@qq.com>
2018-04-19 16:14:48 +0800

    添加单元测试

--- src/test/java/TsetMain.java
+++ src/test/java/TsetMain.java
@@ -1,7 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TsetMain {
 
+    @Test
     public void test() {
         System.out.println("test");
+        Assert.assertTrue(true);
     }
 
 }
